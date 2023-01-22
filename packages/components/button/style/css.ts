e627cc2c251833addf7967c49785ddfa16f4f241
huccct <2972223145@qq.com>
2023-01-22 17:52:41 +0800

    Update By TuNanChen

--- packages/components/button/style/css.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Description: Stay hungry，Stay foolish
- * @Author: Huccct
- * @Date: 2023-01-22 16:37:22
- * @LastEditors: Huccct
- * @LastEditTime: 2023-01-22 16:44:42
- */
--- packages/components/button/style/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Description: Stay hungry，Stay foolish
- * @Author: Huccct
- * @Date: 2023-01-22 16:37:17
- * @LastEditors: Huccct
- * @LastEditTime: 2023-01-22 16:42:55
- */
-import '@echo-ui/theme-chalk/button.scss';
