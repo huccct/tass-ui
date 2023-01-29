098c3dd45b3f3af1fea639ff377f88a9307091a2
liasazhang <1105739862@example.com>
2023-01-29 16:40:36 +0800

    fix:修改collapse的错误并实现其功能
    build:package.json删除了vite-plugin的依赖

--- packages/components/collapse/index.ts
+++ packages/components/collapse/index.ts
@@ -1,12 +1,11 @@
 import collapse from './src/collapse.vue';
-import panel from './src/panel.vue';
-import { withInstall } from '@echo-ui/utils/withInstall';
 
-collapse.panel = panel
+import { withInstall } from '@echo-ui/utils/withInstall';
 
 
 
 export const EchoCollapse = withInstall(collapse);
-export const Panel = withInstall(panel);
 
-export default EchoCollapse;
+
+
+export default {EchoCollapse};
--- packages/components/collapse/src/collapse.vue
+++ packages/components/collapse/src/collapse.vue
@@ -1,109 +1,68 @@
 <template>
-  <div :class="classes">
-    <slot></slot>
+  <!--手风琴组件-->
+  <div>
+    <ul>
+      <li>
+        <div class="title" @click="toggle(1)">标题一</div>
+        <transition name="slide">
+          <div class="content" v-show="show===1">
+            <slot name="one"></slot>
+          </div>
+        </transition>
+      </li>
+      <li>
+        <div class="title" @click="toggle(2)">标题二</div>
+        <transition name="slide">
+          <div class="content" v-show="show===2">
+            <slot name="two"></slot>
+          </div>
+        </transition>
+      </li>
+      <li>
+        <div class="title" @click="toggle(3)">标题三</div>
+        <transition name="slide">
+          <div class="content" v-show="show===3">
+            <slot name="three"></slot>
+          </div>
+        </transition>
+      </li>
+    </ul>
   </div>
 </template>
+ 
+<script lang="ts" setup name="EchoCollapse">
+import { computed, onMounted, onUnmounted, ref } from 'vue';
+const show = ref(0);
+ 
+function toggle(index) {
+      // 获取index 来判断当前点击并展示
+      if(this.show==index) this.show=0;
+      else
+      this.show = index;
+};
+  
 
-<script>
-const prefixCls = "ka-collapse";
-export default {
-  name: "collapse",
-  props: {
-    // 是否是手风琴模式
-    accordion: {
-      type: Boolean,
-      default: false
-    },
-    // 传递进来的值
-    value: [Array, String]
-  },
-  computed: {
-    classes() {
-      // `${prefixCls-simple}` 是变量需要添加[]
-      return [`${prefixCls}`];
-    }
-  },
-  data() {
-    return {
-      currentValue: this.value // 传递进来当前的值
-    };
-  },
-  mounted() {
-    this.setActive();
-  },
-  methods: {
-    setActive() {
-      // 为它下面的子元素都设置一个index值
-      // console.log("setActive");
-      const activeKey = this.getActiveKey();
-      this.$children.forEach((child, index) => {
-        const name = child.name || index.toString(); // toString 1=>"1"整数转换成为字符串
-        child.isActive = activeKey.indexOf(name) > -1; // 给选中的元素赋值活跃状态
-        // console.log(child);
-        child.index = index;
-      });
-    },
-    toggle(data) {
-      // console.log("toggle");
-      const name = data.name.toString(); // 强行转换成为字符串
-      let newActivekey = [];
-      if (this.accordion) {
-        // 如果是手风琴模式
-        if (!data.isActive) {
-          newActivekey.push(name);
-        }
-      } else {
-        let activeKey = this.getActiveKey();
-        const nameIndex = activeKey.indexOf(name);
-        if (data.isActive) {
-          // 如果当前是展开状态
-          if (nameIndex > -1) {
-            activeKey.splice(nameIndex, 1);
-          }
-        } else {
-          if (nameIndex < 0) {
-            activeKey.push(name);
-          }
-        }
-        newActivekey = activeKey;
-      }
-      this.currentValue = newActivekey;
-      // console.log(data);
-      this.$emit("input", newActivekey);
-      this.$emit("on-change", newActivekey);
-    },
-    getActiveKey() {
-      // 获取当前展开的元素，并且做成数组的形式 1 => ["1"]
-      let activeKey = this.currentValue || [];
-      const accordion = this.accordion;
-      if (!Array.isArray(activeKey)) {
-        // 判断 activeKey 是不是数组
-        activeKey = [activeKey]; // 不是数组则让它变成数组
-      }
-      if (accordion && activeKey.length > 1) {
-        // 如果是手风琴模式，必定是只会有一个元素
-        activeKey = [activeKey[0]];
-      }
-
-      for (let i = 0; i < activeKey.length; i++) {
-        activeKey[i] = activeKey[i].toString();
-      }
-      return activeKey;
-    }
-  },
-  watch: {
-    value(val) {
-      console.log(val);
-      this.currentValue = val;
-    },
-    currentValue() {
-      console.log("currentValue");
-      this.setActive();
+</script>
+<style scoped lang="scss">
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+    .title {
+    height: 50px;
+    line-height: 50px;
+    padding-left: 10px;
+    border: 1px solid #c0c0c1;
+    border-radius: 10px;
+    cursor: pointer;
+      .content {
+      padding-left: 30px;
+      background: rgb(250, 247, 247);
+      color: rgb(56, 57, 59);
     }
   }
-};
-</script>
+}
 
-<style lang="scss" scoped>
-// border-top: 1px solid #dcdee2;
+
+
 </style>
