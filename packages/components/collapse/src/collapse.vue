925020ee59665dc5044f88860a8135cb26cc6cc9
liasazhang <1105739862@example.com>
2023-01-31 21:28:10 +0800

    refactor:基于bem规范对collapse的scss代码进行规范重构

--- packages/components/collapse/src/collapse.vue
+++ packages/components/collapse/src/collapse.vue
@@ -7,31 +7,31 @@
 -->
 <template>
   <!--手风琴组件-->
-  <div>
-    <ul>
+  <div class="ec-collapse">
+    <ul class="ec-collapse-ul">
       <li>
-        <div class="title" @click="toggle(1)">标题一</div>
-        <transition name="slide">
-          <div class="content" v-show="show === 1">
+        <div class="ec-collapse-title" @click="toggle(1)">标题一</div>
+        <ec-collapse-transition name="slide">
+          <div class="ec-collapse-content" v-show="show === 1">
             <slot name="one"></slot>
           </div>
-        </transition>
+        </ec-collapse-transition>
       </li>
       <li>
-        <div class="title" @click="toggle(2)">标题二</div>
-        <transition name="slide">
-          <div class="content" v-show="show === 2">
+        <div class="ec-collapse-title" @click="toggle(2)">标题二</div>
+        <ec-collapse-transition name="slide">
+          <div class="ec-collapse-content" v-show="show === 2">
             <slot name="two"></slot>
           </div>
-        </transition>
+        </ec-collapse-transition>
       </li>
       <li>
-        <div class="title" @click="toggle(3)">标题三</div>
-        <transition name="slide">
-          <div class="content" v-show="show === 3">
+        <div class="ec-collapse-title" @click="toggle(3)">标题三</div>
+        <ec-collapse-transition name="slide">
+          <div class="ec-collapse-content" v-show="show === 3">
             <slot name="three"></slot>
           </div>
-        </transition>
+        </ec-collapse-transition>
       </li>
     </ul>
   </div>
