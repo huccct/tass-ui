208dd27609d175ac90b120a78c124010129a2bec
huccct <2972223145@qq.com>
2023-02-03 14:16:10 +0800

    docs(初始化配置文件): 初始化配置文件

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/* https://commitlint.js.org/#/reference-rules */
+module.exports = {
+  ignores: [commit => commit.includes('init')],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0]
+  }
+};
