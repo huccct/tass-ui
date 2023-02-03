b9aac6e247881affc41f78bbfa7d2d9627e99e26
949516815 <949516815@qq.com>
2023-02-03 19:45:35 +0800

    fix:修复message的bug.

--- packages/components/message/src/message.ts
+++ packages/components/message/src/message.ts
@@ -2,14 +2,16 @@
  * @Description: Stay hungry，Stay foolish
  * @Author: Huccct
  * @Date: 2023-01-25 22:01:00
- * @LastEditors: Huccct
- * @LastEditTime: 2023-01-26 14:32:38
+ * @LastEditors: YuShu Xiao
+ * @LastEditTime: 2023-02-03 19:41:08
  */
 import type { IMessageOptions } from './message.type';
 import message from './message.vue';
 import { createVNode, render } from 'vue';
 const instance: any[] = [];
+let count = 0;
 export default function Message(options: IMessageOptions) {
+  count++;
   if (typeof options === 'string') {
     options = {
       message: options
@@ -19,6 +21,11 @@ export default function Message(options: IMessageOptions) {
   instance.forEach(() => {
     offset += 60;
   });
+
+  if (count === 1) {
+    offset = 0;
+  }
+
   const userClose = options.onclose;
   const ops = {
     ...options,
@@ -29,9 +36,14 @@ export default function Message(options: IMessageOptions) {
   };
   const vm = createVNode(message, ops);
   const container = document.createElement('div');
+
   vm.props!.onDestroy = () => {
+    count--;
+    instance.pop();
     render(null, container);
   };
+  console.log(count);
+
   render(vm, container);
   document.body.appendChild(container.firstElementChild!);
   instance.push(vm);
