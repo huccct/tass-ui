dd4e2d6c8dfbffcd3f266f5713fdc2c6fb464c76
huccct <2972223145@qq.com>
2023-02-05 17:53:55 +0800

    feat(input): 更新了input功能

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="tas-input">
+  <div class="tas-input" :class="styleClass">
     <input
       class="tas-input__inner"
       placeholder="请输入内容"
       :value="inputProps.modelValue"
       @input="changeInputVal"
+      :disabled="inputProps.disabled"
     />
   </div>
 </template>
 <script setup lang="ts" name="TassInput">
+  import { computed } from 'vue';
+
   type InputProps = {
     modelValue?: string | number;
+    disabled?: boolean;
   };
 
   type InputEmits = {
@@ -23,5 +27,10 @@
   const changeInputVal = (event: Event) => {
     inputEmits('update:modelValue', (event.target as HTMLInputElement).value);
   };
+  const styleClass = computed(() => {
+    return {
+      'is-disabled': inputProps.disabled
+    };
+  });
 </script>
 <style></style>
