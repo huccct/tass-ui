064a7a4c92e3c535241cf065c915ec3720d943f6
huccct <2972223145@qq.com>
2023-02-05 16:24:54 +0800

    chore(*): 打包前准备

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     target: 'modules',
@@ -40,5 +41,16 @@ export default defineConfig({
       formats: ['es', 'cjs']
     }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    dts({
+      //指定使用的tsconfig.json为我们整个项目根目录下掉,如果不配置,你也可以在components下新建tsconfig.json
+      tsConfigFilePath: '../../tsconfig.json'
+    }),
+    //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
+    dts({
+      outputDir: 'lib',
+      tsConfigFilePath: '../../tsconfig.json'
+    })
+  ]
 });
