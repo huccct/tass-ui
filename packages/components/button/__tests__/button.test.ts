b40aea0ca17e0bf1f2eb472c4b6a04b2fcb311ae
huccct <2972223145@qq.com>
2023-02-06 14:16:23 +0800

    fix(*): 修复一些bug2

--- packages/components/button/__tests__/button.test.ts
+++ packages/components/button/__tests__/button.test.ts
@@ -3,13 +3,14 @@
  * @Author: Huccct
  * @Date: 2023-01-22 16:51:31
  * @LastEditors: Huccct
- * @LastEditTime: 2023-02-06 13:27:44
+ * @LastEditTime: 2023-02-06 14:15:27
  */
 import { describe, expect, it } from 'vitest';
 
 import { mount } from '@vue/test-utils';
 import button from '../src/button.vue';
 // The component to test
+
 describe('test Button', () => {
   it('should render slot', () => {
     const wrapper = mount(button, {
@@ -20,5 +21,13 @@ describe('test Button', () => {
 
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain('Hello world');
+  });
+  it('should have class', () => {
+    const wrapper = mount(button, {
+      props: {
+        type: 'primary'
+      }
+    });
+    expect(wrapper.classes()).toContain('tas-button--primary');
   });
 });
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -3,7 +3,7 @@
  * @Author: Huccct
  * @Date: 2023-02-05 16:01:16
  * @LastEditors: Huccct
- * @LastEditTime: 2023-02-06 13:28:09
+ * @LastEditTime: 2023-02-06 14:05:39
  */
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
@@ -56,8 +56,8 @@ export default defineConfig({
     dts({
       entryRoot: 'components',
       outputDir: [
-        resolve(__dirname, '../../build/es/src/dts'),
-        resolve(__dirname, '../../build/lib/src/dts')
+        resolve(__dirname, '../../build/es/src'),
+        resolve(__dirname, '../../build/lib/src')
       ],
       //指定使用的tsconfig.json为我们整个项目根目录下掉,如果不配置,你也可以在components下新建tsconfig.json
       tsConfigFilePath: '../../tsconfig.json'
