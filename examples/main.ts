49f35362d08812f843928ff9a07e1c60d5917095
huccct <2972223145@qq.com>
2023-02-07 23:28:14 +0800

    fix(*): bug fix

--- examples/main.ts
+++ examples/main.ts
@@ -3,12 +3,12 @@
  * @Author: Huccct
  * @Date: 2023-01-21 11:16:00
  * @LastEditors: Huccct
- * @LastEditTime: 2023-02-07 21:01:13
+ * @LastEditTime: 2023-02-07 23:26:04
  */
 import { createApp } from 'vue';
 import App from './app.vue';
 import router from './router';
-import '@tass-ui/theme-chalk/src/index.scss';
+// import '@tass-ui/theme-chalk/src/index.scss';
 import TassUI from '@tass-ui/components';
 
 const app = createApp(App);
