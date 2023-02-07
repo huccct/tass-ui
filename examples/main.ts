6c535b9905b4848a6e34f46a234f71957ea46a69
huccct <2972223145@qq.com>
2023-02-07 21:15:48 +0800

    fix(*): 修复bug

--- examples/main.ts
+++ examples/main.ts
@@ -2,16 +2,19 @@
  * @Description: Stay hungry，Stay foolish
  * @Author: Huccct
  * @Date: 2023-01-21 11:16:00
- * @LastEditors: YuShu Xiao
- * @LastEditTime: 2023-02-07 16:13:03
+ * @LastEditors: Huccct
+ * @LastEditTime: 2023-02-07 21:01:13
  */
 import { createApp } from 'vue';
 import App from './app.vue';
-import router from "./router";
+import router from './router';
 import '@tass-ui/theme-chalk/src/index.scss';
-import TassUI from '@tass-ui/components';
+import TassUI from '../build';
+import TassUI1 from '../build/es/components';
+
+console.log(TassUI === TassUI1);
 const app = createApp(App);
 
 app.use(TassUI as any);
-app.use(router)
+app.use(router);
 app.mount('#app');
