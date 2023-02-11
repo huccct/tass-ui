e1da95a843484f6a42f4d2a857fc7045d2d138c8
huccct <2972223145@qq.com>
2023-02-11 20:07:31 +0800

    docs(pagination): 文档更新

---
+++ docs/components/pagination/maxSize.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <tass-pagination
+      @change-page="changePage"
+      :pagesize="pageSize"
+      :total="total"
+      :page="1"
+    ></tass-pagination>
+  </div>
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const total = ref(6);
+  const pageSize = ref(1);
+  const changePage = (page: number) => {};
+</script>
+<style></style>
