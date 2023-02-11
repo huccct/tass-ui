0bbfba9ff20a69016562d31f486322efa02544ac
huccct <2972223145@qq.com>
2023-02-11 10:10:17 +0800

    feat(pagination): 新增pagination组件

---
+++ packages/components/pagination/src/pagination.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Description: Stay hungry，Stay foolish
+ * @Author: Huccct
+ * @Date: 2023-02-11 09:35:12
+ * @LastEditors: Huccct
+ * @LastEditTime: 2023-02-11 09:35:32
+-->
+<template></template>
+<script setup lang="ts"></script>
+<style></style>
