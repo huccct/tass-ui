e21315f71d4df9487ce512e6e97da33bbc6d1835
huccct <2972223145@qq.com>
2023-02-11 22:29:51 +0800

    docs(checkbox): 更新文档

---
+++ docs/components/checkbox/funct.vue
@@ -0,0 +1,11 @@
+<template>
+  <tass-checkbox v-model="checked1" @change="handlerChange">Option 1</tass-checkbox>
+</template>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const checked1 = ref(true);
+  const handlerChange = () => {
+    alert('This is a handleChange test.');
+  };
+</script>
+<style></style>
