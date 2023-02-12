9bb731aefaa69b922776cc4e0a5a7699a7ceb326
huccct <2972223145@qq.com>
2023-02-12 13:43:16 +0800

    Add design ideas
    we wrote this together~
    
    Co-Authored-By: xysccc <99579020+xysccc@users.noreply.github.com>

--- packages/components/date-picker/src/date-picker.vue
+++ packages/components/date-picker/src/date-picker.vue
@@ -3,13 +3,23 @@
  * @Author: Huccct
  * @Date: 2023-02-12 10:57:19
  * @LastEditors: Huccct
- * @LastEditTime: 2023-02-12 11:59:57
+ * @LastEditTime: 2023-02-12 13:25:21
 -->
 <template>
   <div class="tas-datepicker">
-    <tass-input prefix-icon="calendarfull" />
-    <div></div>
+    <tass-input prefix-icon="calendarfull" @focus="onFocus" @blur="onBlur" />
+    <div class="tas-picker" v-if="isVisible"></div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import '../style/';
+  const isVisible = ref(false);
+  const onFocus = () => {
+    isVisible.value = true;
+  };
+  const onBlur = () => {
+    isVisible.value = false;
+  };
+</script>
 <style></style>
