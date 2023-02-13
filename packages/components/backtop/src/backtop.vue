c390ae10d801043c0e5963fb0dc84327532121c6
YuShuXiao <949516815@qq.com>
2023-02-13 13:36:23 +0800

    fix(backtop): bug修改

--- packages/components/backtop/src/backtop.vue
+++ packages/components/backtop/src/backtop.vue
@@ -1,16 +1,19 @@
 <template>
-  <div v-show="btnShow" class="tas-backtop">
-    <tass-icon name="rocket"  class="tas-backtop-icon" :src="BackTop"  @click="backTop"></tass-icon>
+  <div v-show="btnShow" class="tas-backtop" @click="backTop">
+    <tass-icon name="rocket"  class="tas-backtop-icon"></tass-icon>
   </div>
 </template>
 
 <script lang="ts" setup name="TassBacktop">
   import '../style/';
-  import {onMounted, ref} from "vue";
+  import {onMounted, ref,onBeforeMount} from "vue";
 
   const btnShow = ref(true);
   onMounted(() => {
     window.addEventListener('scroll', scrollToTop);
+    window.addEventListener('beforeunload', (e) => {
+      window.scroll(0, 0);
+    });
   });
   let timer = null;
   function backTop() {
@@ -18,7 +21,7 @@
       let backTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
       let speedTop = backTop / 10;
       document.documentElement.scrollTop = backTop - speedTop;
-      if(backTop === 0){
+      if(Math.abs(backTop) < 1){
         clearInterval(timer)
       }
     }, 10);
