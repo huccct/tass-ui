8ce29b0fd401268f552af4bee46b50365cb7a4ab
Chansinging <1183520536@qq.com>
2023-02-16 14:22:23 +0800

    fix(*): 空白

---
+++ packages/components/dropdown/src/interface.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @name: interface.d.ts
+ * @author: cxy
+ * @date: 2023/2/12 18:16
+ * @description：interface.d.ts
+ * @update: 2023/2/12 18:16
+ */
+export type DropdownSize =  'medium' | 'small' ;
